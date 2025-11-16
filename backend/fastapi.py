fde0b76236a0dcda990c6a7cd9e74c43feeeb1a4
moggs2 <mo.ggs@outlook.com>
2025-11-16 23:01:55 +0100

    Update fastapi.py
    Added some ins and outs.

--- backend/fastapi.py
+++ backend/fastapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_login import LoginManager 
@@ -6,16 +6,16 @@ from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi import Depends, Request, UploadFile
 import json, os
 from google import genai
-from models import teacher
-from services import teacher_service
-from config.settings import settings
+from models import teacher, lesson_plan, performance_overview, recommendation, weak_area
+from services import teacher_service, lesson_plan_service, performance_service, recommendation_service, weak_area_service
 
 
 app = FastAPI()
 
 
-SECRET = settings.FASTAPI_SECRET
-os.environ["GEMINI_API_KEY"] = settings.GEMINI_API_KEY
+SECRET = "FirstSecretWord"
+teacher_id = "VirtualTour"
+os.environ["GEMINI_API_KEY"] = 'YOUR_API_KEY'
 
 
 client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
@@ -42,6 +42,84 @@ async def submit_teacher_form(name: str = Form(...), email: str = Form(...)):
     newteacher = teacher.Teacher(name=name, email=email)
     teacher_service.TeacherService.save_teacher(newteacher)
     return {"Form received"}
+
+
+@app.post("/get_teacher/")
+async def get_teacher():
+    teacher =teacher_service.TeacherService.get_teacher(teacher_id)
+    return {json.dumps(teacher)}
+
+
+@app.post("/update_teacher/")
+async def teacher_update(field: str = Form(...), value: str = Form(...)):
+    teacher_service.TeacherService.update_field(teacher_id, field, value)
+    return {"Form received"}
+
+
+@app.post("/add_lesson/")
+async def add_lesson_form(lessonplan: lesson_plan.LessonPlan):
+    lesson_plan_service.LessonPlanService.add_lesson_plan(teacher_id, lessonplan)
+    return {"Form received"}
+
+
+@app.post("/save_lesson/")
+async def save_lesson_form(plan_id: str = Form(...), lessonplan: lesson_plan.LessonPlan = Depends()):
+    lesson_plan_service.LessonPlanService.save_lesson_plan(teacher_id, plan_id, lessonplan)
+    return {"Form received"}
+
+
+@app.post("/list_lessons/")
+async def list_lessons():
+    lessons = lesson_plan_service.LessonPlanService.list_lesson_plans(teacher_id)
+    return {json.dumps(lessons)}
+
+
+@app.post("/performance_save/")
+async def performance_save(performance: performance_overview.PerformanceOverview):
+    performance_service.PerformanceService.save_overview(teacher_id, performance)
+    return {"Form received"}
+
+
+@app.post("/performance_overview/")
+async def list_performances():
+    performances = performance_service.PerformanceService.get_overview(teacher_id)
+    return {json.dumps(performances)}
+
+
+@app.post("/add_recommendation/")
+async def add_recommendation_form(recommendation: recommendation.Recommendation):
+    recommendation_service.RecommendationService.add_recommendation(teacher_id, recommendation)
+    return {"Form received"}
+
+
+@app.post("/save_recommendation/")
+async def save_recommendation_form(rec_id: str = Form(...), recommendation: recommendation.Recommendation = Depends()):
+    recommendation_service.RecommendationService.save_recommendation(teacher_id, rec_id, recommendation)
+    return {"Form received"}
+
+
+@app.post("/list_recommendations/")
+async def list_recommendations():
+    recommendations = recommendation_service.RecommendationService.list_recommendations(teacher_id)
+    return {json.dumps(recommendations)}
+
+
+@app.post("/add_weak_area/")
+async def add_weak_area_form(weakarea: weak_area.WeakArea):
+   weak_area_service.WeakAreaService.add_weak_area(teacher_id, weakarea)
+    return {"Form received"}
+
+
+@app.post("/save_weak_area/")
+async def save_weak_area_form(topic_id: str = Form(...), weakarea: weak_area.WeakArea = Depends()):
+    weak_area_service.WeakAreaService.save_weak_area(teacher_id, topic_id, weakarea)
+    return {"Form received"}
+
+
+@app.post("/list_weak_areas/")
+async def list_weak_areas():
+    weakareas = weak_area_service.WeakAreaService.list_weak_areas(teacher_id)
+    return {json.dumps(weakareas)}
 
 
 @app.post("/auth/login")
